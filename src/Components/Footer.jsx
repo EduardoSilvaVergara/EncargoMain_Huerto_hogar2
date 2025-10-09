fadf038a4dc5e014a70726b55040a414b50f8c06
eduardo silva <ed.silvav@duocuc.cl>
2025-10-09 20:21:55 -0300

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import paryment1 from './../assets/payment-1.svg'
+import paryment2 from './../assets/payment-2.svg'
+import paryment3 from './../assets/payment-3.svg'
+import paryment4 from './../assets/payment-4.svg'
+import paryment5 from './../assets/payment-5.svg'
+import paryment6 from './../assets/payment-6.svg'
+
+
 function Footer() {
   return (
     <>
