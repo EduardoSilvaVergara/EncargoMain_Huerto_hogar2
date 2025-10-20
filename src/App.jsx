1ec3f94492e41f374f9f57bf1c622825d674a3a3
FabrG <fabr.gonzalezr@duocuc.cl>
2025-10-19 22:47:43 -0300

    App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import About from './Pages/About'
+import Blog from './Pages/Blog'
+import Contact from './Pages/Contact'
+import ProductDetails from './Pages/ProductDetails'
+import ProductDetailsFrutas from './Pages/ProductDetailsFrutas'
+import ProductDetailsOrg from './Pages/ProductDetailsOrg'
+import ProductDetailsVerduras from './Pages/ProductDetailsVerduras'
+import ProductDetailsLacteos from './Pages/ProductDetailsLacteos'
+import Shop from './Pages/Shop'
+import ShopFrutas from './Pages/ShopFrutas'
+import ShopLacteos from './Pages/ShopLacteos'
+import ShopVerduras from './Pages/ShopVerduras'
+import ShopOrg from './Pages/ShopOrg'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
+
+        <Route path='/product/:id' element={<ProductDetails />}/>    
+        <Route path='/productFrutas/:id' element={<ProductDetailsFrutas />}/>
+        <Route path='/productOrganicos/:id' element={<ProductDetailsOrg />}/>
+        <Route path='/productVerduras/:id' element={<ProductDetailsVerduras />}/>
+        <Route path='/productLacteos/:id' element={<ProductDetailsLacteos />}/>        
+
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/shopFrutas' element={<ShopFrutas />} />
+        <Route path='/shopOrg' element={<ShopOrg />} />
+        <Route path='/shopVerduras' element={<ShopVerduras />} />
+        <Route path='/shopLacteos' element={<ShopLacteos />} />
+        
+      </Routes>
+      <Footer />
     </>
   )
 }
